4e0a8692a94f5a49ec4be4fd1a55d4e0ace8fdb0
Samir Tine <samir.tine@luart.org>
2022-06-26 15:19:04 +0200

    Fixed: Removed redondant comments from weather.lua example

--- examples/weather.lua
+++ examples/weather.lua
@@ -2,10 +2,6 @@
 -- http://www.apache.org/licenses/LICENSE-2.0
 -- Execute in Windows Terminal, rather than cmd.exe, for full UTF8 support
 
--- wttr.in Http web service, by Igor Chubin
--- http://www.apache.org/licenses/LICENSE-2.0
--- Execute in Windows Terminal, rather than cmd.exe, for full UTF8 support
-
 local net = require "net"
 local console = require "console"
 
