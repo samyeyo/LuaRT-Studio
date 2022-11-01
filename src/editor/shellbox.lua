48fe7fde2d84f57befeea7c50b8f27c2da41b552
Samir Tine <samir.tine@luart.org>
2022-11-01 09:19:01 +0100

    New local console welcome message with _ARCH

--- src/editor/shellbox.lua
+++ src/editor/shellbox.lua
@@ -530,7 +530,7 @@ function ShellExecuteCode(code, ismacro)
 end
 
 local function displayShellIntro()
-  DisplayShellMsg(TR("Welcome to the interactive Lua 5.4 interpreter").."\n"
+  DisplayShellMsg(TR("Welcome to the interactive Lua 5.4.4 "..console.getvalue("_ARCH").." interpreter").."\n"
     ..TR("Use 'help' to display the local console help.").."\n")
   DisplayShellPrompt('')
 end
