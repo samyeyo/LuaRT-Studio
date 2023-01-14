ab71df01b9b5a571eb16bae55cab7c4a7b33bc16
Samir Tine <samir.tine@luart.org>
2023-01-14 19:17:05 +0100

    Revert back to launch LuaRT interpreter from another process

--- interpreters/desktop.lua
+++ interpreters/desktop.lua
@@ -33,7 +33,7 @@ return {
         end
     end
     local params = self:GetCommandLineArg("lua")
-    local cmd = '"'..exe..'" '..([[-e "io.stdout:setvbuf('no')" "%s"]]):format(filepath)..(params and " "..params or "")
+    local cmd = '"'..exe..'" -e '..'"'.."sys.cmd('"..exe:gsub("\\", "/").." "..(filepath):gsub("\\", "/")..(params and " "..params or "")..[[', false)"]]
 
     -- modify LUA_CPATH and LUA_PATH to work with other Lua versions
     local envcpath = "LUA_CPATH"
